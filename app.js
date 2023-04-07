f1987f10725911a8d75fb7bd85e4dd035ae94ae1
Amine <amine.touba59@gmail.com>
2023-04-07 10:55:19 +0200

    add express and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://aminetouba59:Altqlmj1qiEY9Nmh@dev-web-p7.5flpr4n.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
